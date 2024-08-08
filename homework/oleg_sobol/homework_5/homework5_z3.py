31cc9d76b693af7c030dde289626b3699b2a6c8e
Oleg Sobol <aleh.yatskavets@gmail.com>
2024-08-09 02:53:54 +0300

    Oleg_Sobol_homework5_done

---
+++ homework/oleg_sobol/homework_5/homework5_z3.py
@@ -0,0 +1,4 @@
+students = ['Ivanov', 'Petrov', 'Sidorov']
+subjects = ['math', 'biology', 'geography']
+result = f"Students {', '.join(students)} study these subjects: {', '.join(subjects)}"
+print(result)
