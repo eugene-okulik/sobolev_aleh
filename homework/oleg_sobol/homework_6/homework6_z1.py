ff88ef6b6bb4e9c7659f5ba8fff4b9194a2bf3fb
Oleg Sobol <aleh.yatskavets@gmail.com>
2024-08-09 22:36:49 +0300

    Oleg_Sobol_homework6_change1

--- homework/oleg_sobol/homework_6/homework6_z1.py
+++ homework/oleg_sobol/homework_6/homework6_z1.py
@@ -1,5 +1,5 @@
 text = (
-    "Etiam tincidunt neque erat, quis molestie enim imperdiet vel. " 
+    "Etiam tincidunt neque erat, quis molestie enim imperdiet vel. "
     "Integer urna nisl, facilisis vitae semper at, dignissim vitae libero"
 )
 words = text.split()
